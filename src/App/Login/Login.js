42cd99fcc399355c1b8b8b4fcadaac119e8bf1c6
N7_Remax <ditritusa@gmail.com>
2020-01-20 17:26:28 +0200

    created template and add logo

--- src/App/Login/Login.js
+++ src/App/Login/Login.js
@@ -3,10 +3,19 @@ import React from 'react';
 import "./login.css"
 
 function Login() {
-  return (
-    <div className="login">
-    </div>
-  );
+    return (
+        <div className="login">
+            <div className="support-section">
+                <div className="logo-section">
+                    <div className="logo"></div>
+                    <span className="label">Technologies</span>
+                </div>
+            </div>
+            <div className="sign-in-section">
+
+            </div>
+        </div>
+    );
 }
 
 export default Login;
