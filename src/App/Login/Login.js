9faa11cc4f9d8a3fed2d39839f38f7760b5c7553
N7_Remax <ditritusa@gmail.com>
2020-01-20 21:19:47 +0200

    fix colors and layots. added buttons

--- src/App/Login/Login.js
+++ src/App/Login/Login.js
@@ -10,6 +10,14 @@ function Login() {
                     <div className="logo"></div>
                     <span className="label">Technologies</span>
                 </div>
+                <div>
+                    <span>Get how-to help and instructions or specific feutures in</span>
+                </div>
+                <div className="buttons-place">
+                    <div className="button">help center</div>
+                    <span>or</span>
+                    <div className="button">support</div>
+                </div>
             </div>
             <div className="sign-in-section">
                 <div><span>Welcome Back</span>
