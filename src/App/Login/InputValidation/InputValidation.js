212ce5319135c36c8eae668eb3a65cc3244e887b
N7_Remax <ditritusa@gmail.com>
2020-01-22 00:00:06 +0200

    added validation for inputs

--- src/App/Login/InputValidation/InputValidation.js
+++ src/App/Login/InputValidation/InputValidation.js
@@ -1,43 +1,47 @@
 import React, { Component } from 'react';
+import { TweenMax } from "gsap";
+
+import './inputValidation.css'
 
 class InputValidation extends Component {
     inputRef = React.createRef();
-    
-    state = { value: '' };
-  
-    validate = () => {
-      const haveError = !!this.valueValidationError;
-      this.inputRef.current.animate([
-        { transform: 'translateX(0px)' }, 
-        { transform: 'translateX(-5px)' },
-        { transform: 'translateX(5xp)' }
-      ], { duration: 100, iterations: 5 });
-      return !haveError;
-    }
-    
-    get valueValidationError() {
-      let error = null;
-      this.props.rules.some(rule => error = rule(this.state.value));
-      return error;
-    }
-    
-    render() {
-      const {
-        valueValidationError, onChange, inputRef,
-        state: { value },
-      } = this;
-      
-      return (
-        <div>
-          <input
-            className="customInput"
-            value={value}
-            onChange={e => this.setState({ value: e.target.value })}
-          />
-          <div ref={inputRef}>{valueValidationError}</div>
-        </div> 
-      );
-    }
-  }
 
-  export default InputValidation
+    state = { value: '' };
+
+    validate = () => {
+        const haveError = !!this.valueValidationError;
+        TweenMax.to(this.inputRef.current, 0.12, { x: "+10", yoyo: true, repeat: 5 });
+        TweenMax.to(this.inputRef.current, 0.12, { x: "-10", yoyo: true, repeat: 5 });
+        return !haveError;
+    }
+
+    get valueValidationError() {
+        let error = null;
+        this.props.rules.some(rule => error = rule(this.state.value));
+        return error;
+    }
+
+    render() {
+        const {
+            valueValidationError, onChange, inputRef,
+            state: { value },
+        } = this;
+        
+        const errorHighlight = valueValidationError && value
+
+        return (
+            <div>
+                <input 
+                    {...this.props}
+                    className={`custom-input ${errorHighlight?'input-error':''}`}
+                    ref={inputRef}
+                    value={value}
+                    onChange={e => this.setState({ value: e.target.value })}
+                />
+                {/* <div>{valueValidationError}</div> */}
+            </div>
+        );
+    }
+}
+
+export default InputValidation
--- src/App/Login/Login.js
+++ src/App/Login/Login.js
@@ -6,54 +6,45 @@ import ValidationRules from './ValidationRules/validationRules'
 
 import "./login.css"
 
-class Login extends Component {  
+class Login extends Component {
 
     myInputRef1 = React.createRef();
-
     myInputRef2 = React.createRef();
-  
+
     onValidatePress = () => {
-      const isAllFiedsValid = true
-        & this.myInputRef1.current.validate()
-        & this.myInputRef2.current.validate();
-      
-      if (isAllFiedsValid) alert('all fields valid');
+        const isAllFiedsValid = true
+            & this.myInputRef1.current.validate()
+            & this.myInputRef2.current.validate();
+
+        if (isAllFiedsValid) alert('all fields valid');
     }
-    
+
     render() {
-      const { myInputRef1, myInputRef2, onValidatePress } = this;
-      
-      return (
-        <div>
-          <InputValidation ref={myInputRef1} rules={ValidationRules.inputEmail} />
-          <InputValidation ref={myInputRef2} rules={ValidationRules.inputPassword} />
-          <button onClick={onValidatePress}>validate</button>
-        </div>
-      );
+        const { myInputRef1, myInputRef2, onValidatePress } = this;
+
+        return (
+            <div className="login">
+                <div className="sign-in-section">
+                    <div className="welcome">
+                        <h2>Welcome Back</h2>
+                        <span>Sign in to continue to IO Technologies</span>
+                    </div>
+                    <div className="form-section">
+                        <form className="form">
+                            <InputValidation type="email" placeholder="Your email" ref={myInputRef1} rules={ValidationRules.inputEmail} />
+                            <InputValidation type="password" placeholder="Password" ref={myInputRef2} rules={ValidationRules.inputPassword} />
+                        </form>
+                    </div>
+                    <div className="confirm-place">
+                        <a onClick={onValidatePress} href="#" className="confirm-button">LOG IN<span class="arrow"> &#10230;</span></a>
+                    </div>
+                    <div className="forgot-password-place">
+                        <span>Forgot your password? <Link to="/forgot-password">Reset</Link></span>
+                    </div>
+                </div>
+            </div>
+        );
     }
-  }
-//     return (
-//         <div className="login">
-//             <div className="sign-in-section">
-//                 <div className="welcome">
-//                     <h2>Welcome Back</h2>
-//                     <span>Sign in to continue to IO Technologies</span>
-//                 </div>
-//                 <div className="form-section">
-//                     <form className="form">
-//                         <input placeholder="Your email" type="email"></input>
-//                         <input placeholder="Password" type="password"></input>
-//                     </form>
-//                 </div>
-//                 <div className="confirm-place">
-//                     <a href="#" className="confirm-button">LOG IN<span class="arrow"> &#10230;</span></a>
-//                 </div>
-//                 <div className="forgot-password-place">
-//                     <span>Forgot your password? <Link to="/forgot-password">Reset</Link></span>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
+}
 
 export default Login;
