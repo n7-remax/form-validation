6cdff97928795a747d58a74840361e43e5947a2f
N7_Remax <ditritusa@gmail.com>
2020-01-22 00:35:20 +0200

    improved rules of password

--- src/App/Login/ForgotPassword/ForgotPassword.js
+++ src/App/Login/ForgotPassword/ForgotPassword.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom'
+import InputValidation from '../InputValidation/InputValidation'
+import ValidationRules from '../ValidationRules/validationRules'
 
 
 import './forgotpassword.css'
 
 
 function ForgotPassword() {
+    const inputEl = useRef(null);
+    const onButtonClick = () => {
+        inputEl.current.validate();
+    }
     return (
         <div className="forgot-password">
             <div className="arrow-place">
-            <Link to="/">&#129120;</Link>
+                <Link to="/">&#129120;</Link>
             </div>
             <div className="heading">
                 <h2>Forgot password?</h2>
             </div>
             <div className="form-section">
                 <form className="form">
-                    <input placeholder="Enter your email" type="email"></input>
+                    <InputValidation type="email" placeholder="Your email" ref={inputEl} rules={ValidationRules.inputEmail} />
                 </form>
             </div>
             <div className="confirm-place">
-                <a href="#" className="confirm-button">Send me instructions</a>
+                <a onClick={onButtonClick} href="#" className="confirm-button">Send me instructions</a>
             </div>
         </div>
     );
